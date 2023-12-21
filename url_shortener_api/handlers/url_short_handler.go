9eb62af747aac41dbb450dd2eeaaa4d815fc7879
rakeshp <patowary.rakesh11@gmail.com>
2023-12-22 01:06:37 +0530

    Implement URL Shortening API

---
+++ url_shortener_api/handlers/url_short_handler.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var URLMap = make(map[string]string)
+
+type URLShortenRequest struct {
+	Destination string `json:"destination"`
+}
+
+type URLShortenResponse struct {
+	ShortURL string `json:"short_url"`
+}
+
+// URLShortener is the http handler to short any valid URL
+func URLShortener(rw http.ResponseWriter, r *http.Request) {
+	var usReq URLShortenRequest
+
+	err := json.NewDecoder(r.Body).Decode(&usReq)
+	if err != nil {
+		http.Error(rw, "Invalid request format", http.StatusBadRequest)
+		return
+	}
+
+	shortKey := generateShortKey(5)
+
+	// Map the input URL to the generated short key
+	URLMap[shortKey] = usReq.Destination
+
+	shortURL := fmt.Sprintf("http://avalaraDomain.com/%s", shortKey)
+
+	response := URLShortenResponse{
+		ShortURL: shortURL,
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(response)
+}
+
+// generateShortKey returns a valid slice of string of length keyLength
+func generateShortKey(keyLength int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[r.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
---
+++ url_shortener_api/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+	"url_shortener_api/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	l := log.New(os.Stdout, "url-shortner", log.LstdFlags)
+	sm := mux.NewRouter()
+
+	sm.HandleFunc("/shortURL", handlers.URLShortener).Methods("PUT")
+
+	http.Handle("/", sm)
+
+	// Create the server
+	s := http.Server{
+		Addr:    "localhost:9090",
+		Handler: sm,
+	}
+
+	go func() {
+		l.Println("Server is up & running on port", s.Addr)
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc) // Graceful shutdown
+}
